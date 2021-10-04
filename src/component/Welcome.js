bbd28c3beeb4822ef2d05957ca554cbe38301022
Moinul Moin <moinulmoin101@gmail.com>
2021-10-05 00:10:01 +0600

    added multiple tests in test suite

--- src/component/Welcome.js
+++ src/component/Welcome.js
@@ -1,10 +1,18 @@
-const Welcome = () => {
-    return (
-        <div>
-            <h1>Welcome to React</h1>
-            <p>This is a simple example of a React component.</p>
-        </div>
-    )
-}
+import { useState } from 'react';
 
-export default Welcome
+const Welcome = () => {
+	const [changeText, setChangeText] = useState(false);
+
+	return (
+		<div>
+			<h1>Welcome to React!</h1>
+			{!changeText && <p>This is a simple React component.</p>}
+			{changeText && <p>Changed successfully!</p>}
+			<button type='button' onClick={() => setChangeText(!changeText)}>
+				Change Text
+			</button>
+		</div>
+	);
+};
+
+export default Welcome;
--- src/component/Welcome.test.js
+++ src/component/Welcome.test.js
@@ -1,8 +1,39 @@
-import {render, screen} from '@testing-library/react'
-import Welcome from './Welcome'
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Welcome from './Welcome';
 
-test('render text in Welcome Comp', ()=> {
-    render(<Welcome />)
-    const text = screen.getByText(/Welcome to React/i)
-    expect(text).toBeInTheDocument()
-})
+describe('Welcome Component', () => {
+	test('renders "Welcome to React" text', () => {
+		render(<Welcome />);
+
+		const output = screen.getByText('Welcome to React', { exact: false });
+		expect(output).toBeInTheDocument();
+	});
+
+	test('renders "React Component" text', () => {
+		render(<Welcome />);
+
+		const output = screen.getByText(/React Component/i);
+		expect(output).toBeInTheDocument();
+	});
+
+	test('renders "successfully" text if button is clicked', () => {
+		render(<Welcome />);
+
+		const button = screen.getByRole('button');
+		userEvent.click(button);
+
+		const output = screen.getByText('successfully', { exact: false });
+		expect(output).toBeInTheDocument();
+	});
+
+	test('doesnt render "React component" test if button is clicked', () => {
+		render(<Welcome />);
+
+		const button = screen.getByRole('button');
+		userEvent.click(button);
+
+		const output = screen.queryByText('React component', { exact: false });
+		expect(output).toBeNull();
+	});
+});
